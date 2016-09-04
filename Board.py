4796bc690fb0c10987ce57e724d573caef069511
die-bold <Dongbold@Dongbold-Laptop.resnet.wisc.edu>
2016-09-04 02:18:43 -0500

    working on board, added images
    Added a blank image, a red tile image, and a black tile image.

--- Board.py
+++ Board.py
@@ -4,23 +4,31 @@ class Board(tk.Tk):
 
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
-        tk.Tk.geometry(self, '500x500+150+150')
+        # tk.Tk.geometry(self, '500x500+150+150')
 
         self.board = tk.Frame(self, bg = 'blue')
         self.board.pack(side='top', fill='both', expand=True)
         # self.board.pack()
 
         self.squares = []
-        # self.create_squares()
+        self.create_squares()
 
     def create_squares(self):
+
+        # frame1 = tk.Frame(self.board, bg = 'red')
+        # # frame1.pack(fill=tk.BOTH, expand=1)
+        # frame1.grid(row=0, column=0, sticky=tk.W+tk.E+tk.N+tk.S)
+        # # frame1.grid(row=0,column=0)
+        # label = tk.Label(frame1, text='test')
+        # label.pack()
 
         for x in range(8):
             self.squares.append([])
             for y in range(8):
-                self.squares[x].append(tk.Frame(self.board, bg = 'red'))
-                self.squares[x][y].pack(side='top', fill='both', expand=True)
-                self.squares[x][y].grid(row=x, column=y)
+                self.squares[x].append(tk.Button(self.board, image='blank.jpg'))
+                # self.squares[x][y].pack(expand=True)
+                print(x, ' and ', y)
+                self.squares[x][y].grid(row=x, column=y, sticky='news')
 
 
 if __name__ == '__main__':
