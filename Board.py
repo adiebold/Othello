5f19dcd1708203d6edced5e919c64128479c05ce
die-bold <Dongbold@Dongbold-Laptop.resnet.wisc.edu>
2016-09-04 02:33:57 -0500

    added buttons and images
    Added buttons with the blank image. Added the blank, red, and black
    images with proper file opening.

--- Board.py
+++ Board.py
@@ -1,17 +1,28 @@
 import tkinter as tk
+from PIL import ImageTk, Image
+import os
 
 class Board(tk.Tk):
 
+
+
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
-        # tk.Tk.geometry(self, '500x500+150+150')
+        tk.Tk.geometry(self, '+100+100')
 
         self.board = tk.Frame(self, bg = 'blue')
         self.board.pack(side='top', fill='both', expand=True)
-        # self.board.pack()
+
+        self.create_button_images()
 
         self.squares = []
         self.create_squares()
+
+    def create_button_images(self):
+
+        self.blank = ImageTk.PhotoImage(Image.open('blank.jpg'))
+        self.red = ImageTk.PhotoImage(Image.open('red.jpg'))
+        self.black = ImageTk.PhotoImage(Image.open('black.jpg'))
 
     def create_squares(self):
 
@@ -25,7 +36,7 @@ class Board(tk.Tk):
         for x in range(8):
             self.squares.append([])
             for y in range(8):
-                self.squares[x].append(tk.Button(self.board, image='blank.jpg'))
+                self.squares[x].append(tk.Button(self.board, image=self.blank))
                 # self.squares[x][y].pack(expand=True)
                 print(x, ' and ', y)
                 self.squares[x][y].grid(row=x, column=y, sticky='news')
