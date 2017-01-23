6506d09bc5f254ec47492876e9070d8bd2e9d3f7
die-bold <Dongbold@Dongbold-Laptop.resnet.wisc.edu>
2017-01-23 16:26:04 -0600

    Re-did program to use a board with an 8x8 array of tiles to improve layout of program

---
+++ tile.py
@@ -0,0 +1,11 @@
+class Tile():
+
+    BLANK = 'blank'
+    RED = 'red'
+    BLACK = 'black'
+
+    def __init__(self, *args, **kwargs):
+        self.status = BLANK
+
+    def print_status():
+        print(self.status)
